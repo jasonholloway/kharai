13acd6c92e85951f1f27eb7c2e7061eaee2d7baa
Jason Holloway <miceeatnicerice@hotmail.com>
2020-02-16 01:04:55 +0000

    locks increment

--- src/Locks.ts
+++ src/Locks.ts
@@ -3,128 +3,156 @@ import {Set, OrderedMap} from 'immutable'
 type Token = object
 type Releasable = () => void;
 type Waiter = () => (((l:Releasable)=>void) | false);
-type DoTheLock = () => Releasable;
+type DoTheInc = () => Releasable;
 
-export type Lock = {
+type Handle = {
 	release(): void
 	extend(extras: Set<object>): void
 }
 
-export default class LockRegistry {
-	private _entries = new WeakMap<object, Entry>();
+export default class Locks {
+	private readonly _defaultCount: number
+	private readonly _entries: WeakMap<object, Entry>
 
-	readonly lock: (...items: object[]) => Promise<Lock> =
-		(...items) => new Promise<Lock>(resolve => {
+	constructor(defaultAvail: number) {
+		this._defaultCount = defaultAvail;
+		this._entries = new WeakMap<object, Entry>();
+	}
+
+  lock(...items: object[]) {
+		return this.inc(items, -1);
+	}
+
+	inc(items: object[], c: number): Promise<Handle> {
+		return new Promise<Handle>(resolve => {
 			const token = new Object();
 
-			const lockAll: (items: Set<object>) => void =
+			const incAll: (items: Set<object>) => void =
 				(items) => {
-					const allLocked = tryLockAllNow(items);
-					if(allLocked) {
-						resolve(createLock(items, allLocked));
+					const allDone = tryIncAllNow(items);
+					if(allDone) {
+						resolve(wrap(items, allDone));
 					}
 					else {
-						const answers = items.map(i => [i, tryLockItem(i)] as const);
+						const answers = items.map(i => [i, tryIncOne(i)] as const);
 						answers.forEach(([i, ans]) => {
-							if(ans[0] == 'mustWait') ans[1](adoptOneLockAll(i, items));
+							if(ans[0] == 'mustWait') ans[1](adoptOneIncAll(i, items));
 						});
 					}
 				};
 
-			const tryLockAllNow: (items: Set<object>) => Set<Releasable>|false =
+			const tryIncAllNow: (items: Set<object>) => Set<Releasable>|false =
 				(items) => {
-					const answers = items.map(tryLockItem);
-					if(answers.every(([m]) => m == 'canLock')) {
-						return answers.map(([,fn]) => (<DoTheLock>fn)());
+					const answers = items.map(tryIncOne);
+					if(answers.every(([m]) => m == 'canAdd')) {
+						return answers.map(([,fn]) => (<DoTheInc>fn)());
 					}
 					else {
 						return false;
 					}
 				};
 
-			const adoptOneLockAll: ((item: object, allItems: Set<object>) => Waiter) =
+			const adoptOneIncAll: ((item: object, allItems: Set<object>) => Waiter) =
 				(item, allItems) => () => {
-					const answers = allItems.subtract([item]).map(i => [i, tryLockItem(i)] as const);
+					const answers = allItems.subtract([item]).map(i => [i, tryIncOne(i)] as const);
 
-					if(answers.every(([,[m]]) => m === 'canLock')) {
-						let locked = answers.map(([,[,fn]]) => (<DoTheLock>fn)());
+					if(answers.every(([,[m]]) => m === 'canAdd')) {
+						const locks = answers.map(([,[,fn]]) => (<DoTheInc>fn)());
 						return lock =>
-							resolve(createLock(allItems, locked.add(lock)));
+							resolve(wrap(allItems, locks.add(lock)));
 					}
 					else {
 						answers.forEach(([i, ans]) => {
-							if(ans[0] == 'mustWait') ans[1](adoptOneLockAll(i, allItems));
+							if(ans[0] == 'mustWait') ans[1](adoptOneIncAll(i, allItems));
 						})
 
 						return false;
 					}
 				}
 
-			const tryLockItem =
-				(item: object) => this.summonEntry(item).tryLock(token);
+			const tryIncOne =
+				(item: object) => this.summonEntry(item).tryInc(token, c);
 
-			const createLock: (items: Set<object>, locked: Set<Releasable>) => Lock =
-				(items, locked) => ({
-					release() { locked.forEach(release => release()); },
+			const wrap: (items: Set<object>, handles: Set<Releasable>) => Handle =
+				(items, handles) => ({
+					release() { handles.forEach(release => release()); },
 					extend(extras) {
-						const locked2 = tryLockAllNow(extras.subtract(items));
+						const locked2 = tryIncAllNow(extras.subtract(items));
 						if(locked2) {
-							locked = locked.union(locked2);
+							handles = handles.union(locked2);
 							items = items.union(extras);
 						}
 						else throw 'can\'t extend onto locked items!';
 					}
 				});
 
-			lockAll(Set(items));
+			incAll(Set(items));
 		})
+	}
 
 	private summonEntry(i: object): Entry {
 		return this._entries.get(i)
 		  || (() => {
-				const created = new Entry()
+				const created = new Entry(this._defaultCount)
 				this._entries.set(i, created);
 				return created;
 			})()
 	}
 
-	test(item: object): boolean {
-		const response = this.summonEntry(item).tryLock(new Object());
-		return response[0] == 'canLock';
+	canInc(item: object, c: number): boolean {
+		const response = this.summonEntry(item).tryInc(new Object(), c);
+		return response[0] == 'canAdd';
+	}
+
+	canLock(item: object): boolean {
+		return this.canInc(item, -1);
 	}
 }
 
 class Entry {
-	private _isLocked = false
-	private _waits = OrderedMap<Token, Waiter>()
+	private _avail: number
+	private _waits: OrderedMap<Token, [number, Waiter]>
 
-	tryLock(k: Token): ['canLock',()=>Releasable] | ['mustWait',(cb:Waiter)=>void] {
-		return !this._isLocked
-		  ? ['canLock', () => {
-					this._isLocked = true;
-					this.removeWait(k);
-					return () => this.unlock();
-				}]
-		  : ['mustWait', cb => {
-					this.addWait(k, cb);
-					return () => this.removeWait(k);
-				}];
+	constructor(avail: number) {
+		this._avail = avail;
+		this._waits = OrderedMap();
 	}
 
-	private unlock() {
-		for(const [k,waiter] of this._waits) {
-			this.removeWait(k);
-			const willAdopt = waiter();
-			if(willAdopt) {
-				willAdopt(() => this.unlock());
-				return;
+	tryInc(k:Token, c: number): ['canAdd',()=>Releasable] | ['mustWait',(cb:Waiter)=>void] {
+		return this.canInc(c)
+		  ? ['canAdd', () => {
+					this.removeWait(k);
+					this.inc(c);
+					return () => this.inc(-c);
+			  }]
+		  : ['mustWait', cb => {
+					this.addWait(k, [c, cb]);
+					return () => this.removeWait(k);
+			  }];
+	}
+
+	private inc(c: number) {
+		this._avail += c;
+
+		for(const [k, [cc, waiter]] of this._waits) {
+			if(this.canInc(cc)) {
+				this.removeWait(k);
+				const willAdopt = waiter();
+				if(willAdopt) {
+					this.inc(cc)
+					willAdopt(() => this.inc(-cc));
+					return;
+				}
 			}
 		}
-		this._isLocked = false;
 	}
 
-	private addWait(k: Token, wait: Waiter) {
-		this._waits = this._waits.set(k, wait);
+	private canInc(c: number) {
+		return this._avail + c >= 0;
+	}
+
+	private addWait(k: Token, tup: [number, Waiter]) {
+		this._waits = this._waits.set(k, tup);
 	}
 	
 	private removeWait(k: Token) {
--- test/locks.test.ts
+++ test/locks.test.ts
@@ -6,86 +6,134 @@ import { List, Set } from 'immutable'
 describe('locks', () => {
 	let run: Chooser;
 
-	let locks: Locks
+	let locks: Locks;
 	const _1 = new Object();
 	const _2 = new Object();
 	const _3 = new Object();
 
 	beforeEach(() => {
 		run = seedChooser(820);
-		locks = new Locks();
 	})
 
-	it('can simply lock and release', async () => {
-		const lock = await locks.lock(_1);
-		lock.release();
-	})
+	describe('as Locks(0), requiring supply', () => {
 
-	it('contention on one object', async () => {
-		let locked2 = false;
+		beforeEach(() => {
+			locks = new Locks(0);
+		})
+
+		it('unavailable by default', () => {
+			const available = locks.canLock(_1);
+			expect(available).toBeFalsy();
+		})
+
+		it('no lock without supply', async () => {
+			let locked = false;
+
+			locks.lock(_1)
+			  .then(() => locked = true);
+
+			await delay(50);
+
+			expect(locked).toBeFalsy();
+		})
+
+		it('lock after supply', async () => {
+			let locked = false;
+			
+			locks.lock(_1)
+			  .then(() => locked = true);
+
+			await locks.inc([_1], 1);
+			
+			await delay(50);
+
+			expect(locked).toBeTruthy();
+		})
 		
-		const lock = await locks.lock(_1);
-
-		locks.lock(_1)
-			.then(() => locked2 = true)
-
-		await delay(10);
-		expect(locked2).toBeFalsy();
-
-		lock.release();
-		await delay(0);
-		expect(locked2).toBeTruthy();
-	})
-
-	it('many objects, partial contention', async () => {
-		let locked2 = false;
-		
-		const lock = await locks.lock(_1, _2)
-
-		locks.lock(_2, _3)
-			.then(() => locked2 = true);
-
-		await delay(50);
-		expect(locked2).toBeFalsy();
-
-		lock.release();
-		await delay(0);
-		expect(locked2).toBeTruthy();
 	})
 	
-	it('contending on two objects', () =>
-		Promise.all([
-			locks.lock(_1).then(lock => delay(100).then(() => lock.release())),
-			locks.lock(_1, _2).then(lock => delay(100).then(() => lock.release())),
-			locks.lock(_2).then(lock => delay(100).then(() => lock.release()))
-		]))
 
-	it('loads of knotty contentions', async () => {
-		const items = [{i:1}, {i:2}, {i:3}, {i:4}, {i:5}];
-		
-		const threads = run(gen(choose => {
-			return choose(many(10, gen(() => ({
-				duration: choose(integer(10, 200)),
-				toLock: choose(pick(0.3, List(items)))
-			}))));
-		}));
+	describe('as Locks(1)', () => {
+		beforeEach(() => {
+			locks = new Locks(1);
+		})
 
-		await Promise.all(
-			threads.map(async ({ duration, toLock }) => {
-				const lock = await locks.lock(...toLock)
-				await delay(duration);
-				lock.release();
+		it('available by default', () => {
+			const available = locks.canLock(_1);
+			expect(available).toBeTruthy();
+		})
+
+		it('can simply lock and release', async () => {
+			const lock = await locks.lock(_1);
+			lock.release();
+		})
+
+		it('contention on one object', async () => {
+			let locked2 = false;
+
+			const lock = await locks.lock(_1);
+
+			locks.lock(_1)
+				.then(() => locked2 = true)
+
+			await delay(10);
+			expect(locked2).toBeFalsy();
+
+			lock.release();
+			await delay(0);
+			expect(locked2).toBeTruthy();
+		})
+
+		it('many objects, partial contention', async () => {
+			let locked2 = false;
+
+			const lock = await locks.lock(_1, _2)
+
+			locks.lock(_2, _3)
+				.then(() => locked2 = true);
+
+			await delay(50);
+			expect(locked2).toBeFalsy();
+
+			lock.release();
+			await delay(0);
+			expect(locked2).toBeTruthy();
+		})
+
+		it('contending on two objects', () =>
+			Promise.all([
+				locks.lock(_1).then(lock => delay(100).then(() => lock.release())),
+				locks.lock(_1, _2).then(lock => delay(100).then(() => lock.release())),
+				locks.lock(_2).then(lock => delay(100).then(() => lock.release()))
+			]))
+
+		it('loads of knotty contentions', async () => {
+			const items = [{i:1}, {i:2}, {i:3}, {i:4}, {i:5}];
+
+			const threads = run(gen(choose => {
+				return choose(many(10, gen(() => ({
+					duration: choose(integer(10, 200)),
+					toLock: choose(pick(0.3, List(items)))
+				}))));
 			}));
+
+			await Promise.all(
+				threads.map(async ({ duration, toLock }) => {
+					const lock = await locks.lock(...toLock)
+					await delay(duration);
+					lock.release();
+				}));
+		})
+
+		it('extending existing lock to include new and non-contentious', async () => {
+			const lock = await locks.lock(_1, _2);
+			lock.extend(Set([_3]));
+			expect(locks.canLock(_3)).toBeFalsy();
+
+			lock.release();
+			expect(locks.canLock(_3)).toBeTruthy();
+		})
 	})
 
-	it('extending existing lock to include new and non-contentious', async () => {
-		const lock = await locks.lock(_1, _2);
-		lock.extend(Set([_3]));
-		expect(locks.test(_3)).toBeFalsy();
-
-		lock.release();
-		expect(locks.test(_3)).toBeTruthy();
-	})
 })
-
 
