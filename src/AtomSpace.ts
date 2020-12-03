46b967ba5850a9ae17f6c6c6ce24cf44c6c91d19
Jason Holloway <miceeatnicerice@hotmail.com>
2020-12-03 23:59:34 +0000

    saver saving; need to fix tests

--- src/AtomSpace.ts
+++ src/AtomSpace.ts
@@ -11,16 +11,14 @@ import Store from './Store';
 
 const log = console.log;
 
+const MonoidLump = <V>() => <_Monoid<Lump<V>>> {
+  zero: [0, Set()],
+  add: ([aW, aS], [bW, bS]) => [aW + bW, aS.union(bS)]
+}
+
 export type Weight = number;
 export type Threshold = number
 export type Commit<V> = [Weight, AtomRef<V>]
-
-// type Weights = { created: number, staged: number, saved: number, pending(): number }
-// type State<V> = { heads: List<Head<V>>, weights: Weights }
-// type Change<V> = (s: State<V>) => State<V>
-
-// type Saver<V> = (refs: Set<AtomRef<V>>) => Observable<Save>
-// type Save = { weight: number }
 
 export type Lump<V> = [Weight, Set<AtomRef<V>>]
 export type Storer<V> = (s: Store<V>) => Promise<any>
@@ -33,32 +31,13 @@ export const runSaver = <V>(signal$: Observable<Signal>, threshold$: Observable<
 
     const space = new AtomSpace<V>();
     const saver = new AtomSaver<V>(mv, space);
-
-    type L = Lump<V>
-    const ML = <_Monoid<L>> {
-      zero: [0, Set()],
-      add: ([aW, aS], [bW, bS]) => [aW + bW, aS.union(bS)]
-    }
-
+    const ML = MonoidLump<V>();
 
     const lump$ = commit$.pipe(
-      map(([w, r]) => <L>[w, Set([r])])
+      map(([w, r]) => <Lump<V>>[w, Set([r])])
     );
 
-    //TODO
-    //stream saves out
-    //the actual save action should be done below
-    //
-    //would be nice if, each time threshold was met,
-    //a function were streamed out, pushing down application of store
-    //this function would then complete the observable that carries it
-    //
-
-    type Tup = [L, Threshold, Storer<V>?]
-
-    //the expand below emits two for every one incoming
-    //hence explosion
-
+    type Tup = [Lump<V>, Threshold, Storer<V>?]
 
     return combineLatest(lump$, threshold$)
       .pipe(
@@ -66,11 +45,11 @@ export const runSaver = <V>(signal$: Observable<Signal>, threshold$: Observable<
           of<Tup>([ML.add(ac,l), t])
             .pipe(
               expand(([[w, rs], t]) =>
-                (w < t)
+                (!w || (w < t))
                   ? EMPTY
                   : new Observable<Tup>(sub => {
                       return sub.next([
-                        [w, rs], t,
+                        ML.zero, 0,
                         async store => {
                           try {
                             const [w2, rs2] = await saver.save(store, rs)
@@ -94,62 +73,10 @@ export const runSaver = <V>(signal$: Observable<Signal>, threshold$: Observable<
 
 export default class AtomSpace<V> {
   private _locks: Locks
-  // private _weights: Weights
-  // private _change$: Subject<Change<V>>
-  // private _atom$: Subject<AtomRef<V>>
-
-  // readonly state$: Observable<State<V>>
-  // readonly atom$: Observable<AtomRef<V>>
 
   constructor() {
     this._locks = new Locks();
-    // this._weights = { created: 0, staged: 0, saved: 0, pending() { return this.created - this.staged } };
-
-    // this._atom$ = new Subject();
-    // this.atom$ = this._atom$;
-
-    // this._change$ = new Subject();
-    // this.state$ = this._change$.pipe(
-    //   scan<Change<V>, State<V>>(
-    //     (ac, c) => c(ac),
-    //     {
-    //       heads: List(),
-    //       weights: { created: 0, staged: 0, saved: 0, pending() { return this.created - this.staged } }
-    //     }),
-    //   shareReplay(1)
-    // );
-
-    // signal$.pipe(
-    //   filter(s => s.stop),
-    //   first(),
-    //   tap(() => {
-    //     this._atom$.complete();
-    //     this._change$.complete();
-    //   })
-    // ).subscribe();
   }
-
-  // incStaged(weight: number) {
-  //   this._weights.staged += weight;
-  //   this._change$.next(s => ({
-  //     ...s,
-  //     weights: {
-  //       ...s.weights,
-  //       staged: s.weights.staged + weight
-  //     }
-  //   }));
-  // }
-
-  // incSaved(weight: number) {
-  //   this._weights.saved += weight;
-  //   this._change$.next(s => ({
-  //     ...s,
-  //     weights: {
-  //       ...s.weights,
-  //       saved: s.weights.saved + weight
-  //     }
-  //   }));
-  // }
 
   async lockPath(...tips: AtomRef<V>[]): Promise<AtomPath<V>> {
     const _tips = Set(tips);
@@ -173,12 +100,4 @@ export default class AtomSpace<V> {
   private lock<V>(atoms: Set<Atom<V>>): Promise<Lock> {
     return this._locks.lock(...atoms);
   }
-
-  // weights() {
-  //   const w = this._weights;
-  //   return {
-  //     ...w,
-  //     pending: w.created - (w.staged + w.saved)
-  //   };
-  // }
 }
