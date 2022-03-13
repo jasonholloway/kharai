fe53bfa2d66612316d07f9ad09760ff12488ec7f
Jason Holloway <miceeatnicerice@hotmail.com>
2022-03-13 14:58:17 +0000

    FacNode lattice

--- src/facs.ts
+++ src/facs.ts
@@ -2,109 +2,58 @@ import { Merge } from "./util";
 
 let nextNodeId = 1;
 
-
-
-export class _FacNode<A, B> {
-
-  private fac: (a:A)=>B
-
-  constructor(fac: (a:A)=>B) {
-    this.fac = fac;
-  }
-  
-  summon(a: A): B { throw 123 }
-
-  static root<T>(): _FacNode<T, T> { return new _FacNode<T, T>((a) => a) };
-
-  // static derive<A, C, D>(parent: FacNode<A, any, C>, fac: (c:C)=>D): FacNode<A, C, D> {
-  //   return new FacNode<A, C, D>(a => parent.summon(a), fac);
-  // }
-
-  static derive<AR extends Many<FacNode<any, any, any>>, C, D>(uppers: AR, fac: (args:ConcatArgs<AR>)=>D): FacNode<NultiplySeeds<AR>, any, D> {
-    return new FacNode<A, C, D>(a => parent.summon(a), fac);
-  }
-}
-
-//
-// root:  .    A->A
-// node1: A->A A->B
-// node2: B->C C->D
-// node3: A->D D->E
-//
-
-
-
-
-
-
-
-export class FacNode<A, B, C> {
+export class FacNode<A, B> {
 
   private readonly nodeId = nextNodeId++;
 
-  private source: (a:A)=>B;
-  private fac: (b:B)=>C;
-  private val: C|undefined;
+  private fac: (a:A, s:Session)=>B;
 
-  constructor(source: (a:A)=>B, derive: (b:B)=>C) {
-    this.source = source;
-    this.fac = derive;
+  constructor(fac: (a:A, s:Session)=>B) {
+    this.fac = fac;
   }
 
-  summon(a: A): C {
-    return this.val || (this.val = this.fac(this.source(a)));
+  summon(a: A) {
+    return this.summonInner(a, new Session());
   }
 
-  static root<A>(): FacNode<A, A, A> { return new FacNode<A, A, A>((a) => a, (a) => a) };
+  private summonInner(a: A, s: Session): B {
+    return s.summon(this.nodeId, () => this.fac(a, s));
+  }
 
-  // static derive<A, C, D>(parent: FacNode<A, any, C>, fac: (c:C)=>D): FacNode<A, C, D> {
-  //   return new FacNode<A, C, D>(a => parent.summon(a), fac);
-  // }
+  static root<T>(): FacNode<T, T> {
+    return new FacNode<T, T>((a) => a)
+  }
 
-  static derive<AR extends Many<FacNode<any, any, any>>, C, D>(uppers: AR, fac: (args:ConcatArgs<AR>)=>D): FacNode<NultiplySeeds<AR>, any, D> {
-    return new FacNode<A, C, D>(a => parent.summon(a), fac);
+  static derive<AR extends readonly FacNode<any, any>[], C>(uppers: AR, fac: (args:ConcatArgs<AR>)=>C): FacNode<MultiplySeeds<AR>, C> {
+    return new FacNode<MultiplySeeds<AR>, C>((a, s) => {
+      const args = <ConcatArgs<AR>>uppers.map(u => u.summonInner(a, s));
+      return fac(args);
+    });
   }
 }
 
-type Many<T, R extends T[] = T[]> = [T, ...R]
+class Session {
 
-type NultiplySeeds<M extends Many<FacNode<any, any, any>>> =
-  M extends [FacNode<infer T, any, any>, ...infer R]
-  ? (R extends Many<FacNode<any, any, any>> ? Merge<T, NultiplySeeds<R>> : T)
+  private entries: [any][] = []
+  
+  summon<T>(id: number, fac: ()=>T): T {
+    const found = this.entries[id];
+
+    if(found) return found[0];
+    else {
+      const val = fac();
+      this.entries[id] = [val];
+      return val;
+    }
+  }
+}
+
+type MultiplySeeds<M> =
+    M extends readonly [] ? unknown
+  : M extends readonly [FacNode<infer T, any>, ...infer R] ? Merge<T, MultiplySeeds<R>>
+  : unknown;
+
+type ConcatArgs<M> =
+    M extends readonly [] ? []
+  : M extends readonly [FacNode<any, infer A>, ...infer R] ? [A, ...ConcatArgs<R>]
   : never;
-
-type ConcatArgs<M extends Many<FacNode<any, any, any>>> =
-  M extends [FacNode<any, any, infer A>, ...infer R]
-  ? (R extends Many<FacNode<any, any, any>> ? [A, ...ConcatArgs<R>] : [A])
-  : never;
-
-
-const root1 = FacNode.root<{ a: string }>();
-const root2 = FacNode.root<{ b: string }>();
-
-const n1 = FacNode.derive([root1, root2], ([a, b]) => 123); 
-n1
-
-
-
-
-
-
-
-
-
-
-//
-// when we build, we capture a memoisable graph  
-// the passing of an A happens up front here - multiple calls will get exactly the same result
-// memoisation requires this immutability
-// if we were to offer different As each time, then how would we memoise?
-// we could only memoise by escaping real memoisation by memoising functions instead of values
-// but we do want to memoise!
-// this is what supports the lattice
-// unchanging upstreams
-// and to concretise as so, we need the root value up front
-
-// so we build, then we summon
-//
-//
--- tests/facs.test.ts
+++ tests/facs.test.ts
@@ -2,10 +2,9 @@ import { FacNode } from "../src/facs";
 
 describe('facs', () => {
 
-
-  it('child node', () => {
+  it('simple', () => {
     const root = FacNode.root<string>();
-    const child = FacNode.derive([root], ([a]) => ({ species: a, sound: 'meeow' }));
+    const child = FacNode.derive([root] as const, ([a]) => ({ species: a, sound: 'meeow' }));
     const result = child.summon('cat');
     expect(result.sound).toBe('meeow');
     expect(result.species).toBe('cat');
@@ -13,41 +12,63 @@ describe('facs', () => {
 
   it('lattice', () => {
     const meet   = FacNode.root<string>();
-    const child1 = FacNode.derive([meet], ([b]) => ({ species: b, sound: 'meeow' }));
-    const child2 = FacNode.derive([meet], ([b]) => ({ species: b, furry: true }));
-    const join   = FacNode.derive([child1, child2], u => u);
+    const child1 = FacNode.derive([meet] as const, ([b]) => ({ species: b, sound: 'meeow' }));
+    const child2 = FacNode.derive([meet] as const, ([b]) => ({ furry: true }));
+    const join   = FacNode.derive([child1, child2] as const, u => Object.assign(u[0], u[1]));
 
-    
-
-    const result1 = child1.build('cat');
+    const result1 = child1.summon('cat');
+    expect(result1.species).toBe('cat');
     expect(result1.sound).toBe('meeow');
 
-    const result2 = child2.summon('lynx');
-    expect(result2.species).toBe('lynx');
-    expect(result2.sound).toBe('shriek');
+    const result2 = child2.summon('cat');
+    expect(result2.furry).toBe(true);
+
+    const result3 = join.summon('cat');
+    expect(result3.furry).toBe(true);
+    expect(result3.sound).toBe('meeow');
+    expect(result3.species).toBe('cat');
   });
 
-  // if we have joins, how does this come about?
-  // each node takes in its upstream
-  // but also its predecessor?
-  // which in most times will be an empty object
-  // this allows extension, but without problematising existing consumers
-  //
-  // 
-  //
-  //
+  it('memoizes shared nodes', () => {
+    let c = 0;
+    
+    const root   = FacNode.root<string>();
+    const meet   = FacNode.derive([root] as const, ([s]) => c++)
+    const child1 = FacNode.derive([meet] as const, ([n]) => n);
+    const child2 = FacNode.derive([meet] as const, ([n]) => n);
+    const join   = FacNode.derive([child1, child2] as const, r => r);
 
+    const result = join.summon('moo');
+    expect(result[0]).toBe(0);
+    expect(result[1]).toBe(0);
+  })
 
+  it('lattice repeated', () => {
+    const meet   = FacNode.root<string>();
+    const child1 = FacNode.derive([meet] as const, ([b]) => ({ species: b, sound: 'meeow' }));
+    const child2 = FacNode.derive([meet] as const, ([b]) => ({ furry: true }));
+    const join   = FacNode.derive([child1, child2] as const, u => Object.assign(u[0], u[1]));
 
-  
+    const result1 = child1.summon('cat');
+    expect(result1.species).toBe('cat');
+    expect(result1.sound).toBe('meeow');
 
-  // memoisation: gah
-  // we have a lattice of upstreams as so often
-  // which means: the diamond problem
-  //
-  // we don't want an upstream to be created repeatedly
-  // but then there is only one summoning, and it must be fresh each time
-  //
-  
+    const result2 = child2.summon('cat');
+    expect(result2.furry).toBe(true);
+
+    const result3 = join.summon('cat');
+    expect(result3.furry).toBe(true);
+    expect(result3.sound).toBe('meeow');
+    expect(result3.species).toBe('cat');
+
+    const result1_2 = child1.summon('dog');
+    expect(result1_2.species).toBe('dog');
+    expect(result1_2.sound).toBe('meeow');
+
+    const result3_2 = join.summon('dog');
+    expect(result3_2.furry).toBe(true);
+    expect(result3_2.sound).toBe('meeow');
+    expect(result3_2.species).toBe('dog');
+  });
 })
 
