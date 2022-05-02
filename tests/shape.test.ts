41bafb368da8a0f8ab33bfd06099dd5bc22047aa
Jason Holloway <miceeatnicerice@hotmail.com>
2022-05-02 11:10:27 +0100

    jerboaized test

--- tests/shape.test.ts
+++ tests/shape.test.ts
@@ -5,7 +5,7 @@ describe('shape', () => {
 
   it('builds node map from tree', () => {
     const w = shape(_ => space({
-      hamster: space({
+      jerboa: space({
         squeak: data(123 as const),
         burrow: data(456 as const),
         syrian: space({
@@ -14,24 +14,24 @@ describe('shape', () => {
       })
     }));
 
-    const r = w.nodes
-    r
+    expect(w.nodes).toBe({
+    })
 
-    expect(w.nodes).toHaveProperty('D_hamster_squeak')
-    expect(w.nodes).toHaveProperty('D_hamster_burrow')
-    expect(w.nodes.D_hamster_syrian_grumpAbout).toBe(789)
+    expect(w.nodes).toHaveProperty('D_jerboa_squeak')
+    expect(w.nodes).toHaveProperty('D_jerboa_burrow')
+    expect(w.nodes.D_jerboa_syrian_grumpAbout).toBe(789)
   })
 
   it('combines node trees', () => {
     const w =
       shape(_ => space({
-        hamster: space({
+        jerboa: space({
           squeak: data(123 as const),
         })
       }))
       .add(
         shape(_ => space({
-          hamster: space({
+          jerboa: space({
             syrian: space({
               grumpAbout: data(789 as const)
             })
@@ -42,57 +42,54 @@ describe('shape', () => {
     const r = w.nodes
     r
 
-    expect(w.nodes).toHaveProperty('D_hamster_squeak')
-    expect(w.nodes.D_hamster_squeak).toBe(123)
-    expect(w.nodes.D_hamster_syrian_grumpAbout).toBe(789)
+    expect(w.nodes).toHaveProperty('D_jerboa_squeak')
+    expect(w.nodes.D_jerboa_squeak).toBe(123)
+    expect(w.nodes.D_jerboa_syrian_grumpAbout).toBe(789)
   })
 
   it('combines nodes', () => {
     const w =
       shape(_ => space({
-        hamster: space({
+        jerboa: space({
           squeak: data(123 as const),
         })
       }))
       .add(
         shape(_ => space({
-          hamster: space({
+          jerboa: space({
             squeak: fac({ hello: 1 })
           })
         }))
       );
 
-    const r = w.nodes
-    r
-
-    expect(w.nodes).toHaveProperty('D_hamster_squeak')
-    expect(w.nodes.D_hamster_squeak).toBe(123)
-    expect(w.nodes.X_hamster_squeak.hello).toBe(1)
+    expect(w.nodes).toHaveProperty('D_jerboa_squeak')
+    expect(w.nodes.D_jerboa_squeak).toBe(123)
+    expect(w.nodes.X_jerboa_squeak.hello).toBe(1)
   })
 
   it('adds facs', () => {
     const w =
       shape(_ => space({
-        hamster: space({
+        jerboa: space({
           squeak: data(123 as const),
         })
       }))
-      .addFac('hamster', () => 1 as const)
+      .addFac('jerboa', () => 1 as const)
 
-    expect(w.nodes.X_hamster).toBe(1)
+    expect(w.nodes.X_jerboa).toBe(1)
   })
 
   it('types facs from upstreams', () => {
     const w =
       shape(_ => space({
-        hamster: space({
+        jerboa: space({
           squeak: data(123 as const),
         })
       }))
       .addFac('', () => ({ a:1 }))
-      .addFac('hamster', u => ({ b: u.a + 1 }))
+      .addFac('jerboa', u => ({ b: u.a + 1 }))
 
-    expect(w.nodes.X_hamster).toBe({ a:1, b:2 });
+    expect(w.nodes.X_jerboa).toBe({ a:1, b:2 });
   })
 })
 
