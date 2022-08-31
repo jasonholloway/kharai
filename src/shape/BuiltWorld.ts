c739518e816bbfa5230eeea9321332d64664772f
Jason Holloway <miceeatnicerice@hotmail.com>
2022-08-31 21:57:33 +0100

    wip

--- src/shape/BuiltWorld.ts
+++ src/shape/BuiltWorld.ts
@@ -1,82 +1,61 @@
 import { List, Map } from "immutable";
 import { Fac, Handler } from "../shapeShared";
-import { Data, formPath, Nodes, separator } from "./common";
-import { NodeVal, Registry } from "./Registry";
+import { Data, formPath, Nodes } from "./common";
+import { Registry } from "./Registry";
 
 export class BuiltWorld<N extends Nodes> {
   public readonly nodes: N = <N><unknown>{}
   public readonly data: Data<N> = <Data<N>><unknown>undefined;
   
-  readonly reg: Registry;
-  readonly nodeMap: Map<string, NodeVal>;
+  readonly nodeMap: Map<string, Found>;
 
-  constructor(reg?: Registry) {
-    this.reg = reg ?? Registry.empty;
+  constructor(reg: Registry) {
+
+    const withFacs = reg.root
+      .mapBreadthFirst<Found>((v, upstreams) => {
+        const parentFac = upstreams.last()?.fac ?? <Fac>((x)=>x);
+        
+        return <Found>{
+          guard: v.guard ? v.guard[0] : undefined, //REALLY THIS SHOULD STILL BE WRAPPED
+          handler: v.handler,
+          fac: _combineFacs(List([parentFac]).concat(v.facs))
+        };
+      }); 
 
     this.nodeMap = Map(
-      reg!.root
-        .mapDepthFirst<List<[List<string>, NodeVal]>>((v, downstream) =>
-          downstream //need to map downstream map too
-            .map((ppl, k) => ppl.map(([pl,v]) => <[List<string>, NodeVal]>[pl.insert(0, k), v]))
+      withFacs
+        .mapDepthFirst<List<[List<string>, Found]>>((v, downstreams) =>
+          downstreams
+            .map((ppl, k) => ppl.map(([pl,v]) => <[List<string>, Found]>[pl.insert(0, k), v]))
             .valueSeq()
             .flatMap(ppl => ppl)
             .toList()
             .push([List(), v]))
         .val
-        .map(([pl,v]) => <[string,NodeVal]>[pl.join(separator), v])
+        .map(([pl,v]) => <[string,Found]>[formPath([...pl]), v])
     );
-  }
 
-  read(address: string): ReadResult {
-    const reg = this.reg;
-    return _read([], address.split(separator));
-
-    function _read(pl: readonly string[], al: readonly string[]): ReadResult {
-      if(al.length) {
-        const [aHead, ...aTail] = al;
-        return _read([...pl, aHead], aTail);
-      }
-
-      const path = formPath(pl);
-
-      return {
-        guard: reg.getGuard(path),
-        handler: reg.getHandler(path),
-        fac: _formFac(List(pl))
-      };
-    }
-
-    function _formFac(pl: List<string>) : Fac {
-      const facs = _findFacs(pl);
-
+    function _combineFacs(facs: List<Fac>): Fac {
       return facs.reduce(
-        (ac, [_,fn]) => x => {
-          const r = ac(x);
-          const r2 = fn(r);
+        (ac, fn) => x => {
+          const r1 = ac(x);
+          if(typeof r1 !== 'object') throw Error('fac not object!');
 
-          if(typeof r === 'object' && typeof r2 === 'object') {
-            return { ...r, ...r2 };
-          }
+          const r2 = fn(r1);
+          if(typeof r2 !== 'object') throw Error('fac not object!');
 
-          throw Error('fac output is not an object...');
+          return { ...r1, ...r2 };
         },
         (x => x));
     }
+  }
 
-    function _findFacs(pl: List<string>): List<readonly [string,Fac]> {
-      if(pl.isEmpty()) return reg.getFacs('').map(f => ['',f] as const);
-
-      const l = _findFacs(pl.butLast());
-
-      const p = formPath([...pl])
-      const r = reg.getFacs(p).map(f => [p,f] as const);
-
-      return List.of(...l, ...r);
-    }
+  read(address: string): Found {
+    return this.nodeMap.get(address, {});
   }
 }
 
-export type ReadResult = {
+export type Found = {
   guard?: any,
   handler?: Handler,
   fac?: Fac
